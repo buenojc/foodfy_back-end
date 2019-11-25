3c104ecaffcd434aeacf5f2e8691fafa984e0a1c
Julio Cesar Bueno <buenojc@outlook.com>
2019-11-25 17:33:06 -0300

    Server working

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const nunjucks = require("nunjucks");
+const server = express();
+
+server.listen(5000, function() {
+  console.log("Server is running");
+});
+
+//Server configuration
+
+server.set("view engine", "html");
+server.use(express.static("../front-end/public"));
+
+nunjucks.configure("../front-end/views", {
+  express: server
+});
+
+// ROUTES
+
+server.get("/", function(req, res) {
+  return res.render("index");
+});
+
+server.get("/sobre", function(req, res) {
+  return res.render("sobre");
+});
+
+server.get("/receitas", function(req, res) {
+  return res.render("receitas");
+});
