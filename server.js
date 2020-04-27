6d82e72bdba79c4cb2a1e39b32acd33f2b59d159
Julio Cesar Bueno <buenojc@outlook.com>
2020-04-27 00:09:30 -0300

    Update Server.js

--- server.js
+++ server.js
@@ -4,36 +4,36 @@ const nunjucks = require("nunjucks");
 const server = express();
 const recipes = require("./data");
 
-server.listen(5000, function() {
+server.listen(5000, function () {
   console.log("Server is running");
 });
 
 //Server configuration
 
 server.set("view engine", "njk");
-server.use(express.static("../front-end/public"));
+server.use(express.static("../frontend/public"));
 
-nunjucks.configure("../front-end/views", {
+nunjucks.configure("../frontend/views", {
   express: server,
   autoescape: false,
-  noCache: true
+  noCache: true,
 });
 
 // ROUTES
 
-server.get("/", function(req, res) {
+server.get("/", function (req, res) {
   return res.render("index", { recipes });
 });
 
-server.get("/sobre", function(req, res) {
+server.get("/sobre", function (req, res) {
   return res.render("sobre");
 });
 
-server.get("/receitas", function(req, res) {
+server.get("/receitas", function (req, res) {
   return res.render("receitas", { recipes });
 });
 
-server.get("/receitas/:index", function(req, res) {
+server.get("/receitas/:index", function (req, res) {
   const recipeIndex = req.params.index;
   const recipe = recipes[recipeIndex];
 
